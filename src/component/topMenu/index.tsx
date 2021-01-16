02ff3ba78859f512c5f5f1e2ecfdea220121cfdb
KinVen-Lee <980962262@qq.com>
2021-01-16 14:05:47 +0800

    refact:修改顶部菜单

--- src/component/topMenu/index.tsx
+++ src/component/topMenu/index.tsx
@@ -1,13 +1,26 @@
 import { Menu, Input, Layout } from "antd";
+import { useState } from "react";
 const { Search } = Input;
 const TopMenu = () => {
+  const [current, setCurrent] = useState("musicLib");
+
+  const handleClick = (e: any) => {
+    setCurrent(e.key);
+  };
+
   const onSearch = (value: string) => {
     console.log(value);
   };
+
   return (
     <>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="musicLib">音乐馆</Menu.Item>
+        <Menu.Item key="mineMusic">我的音乐</Menu.Item>
+
+      </Menu>
+      {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
         <Menu.Item key="1">音乐馆</Menu.Item>
         <Menu.Item key="2">我的音乐</Menu.Item>
         <Search
@@ -16,7 +29,7 @@ const TopMenu = () => {
           onSearch={onSearch}
           style={{ width: 200, margin: "0 10px" }}
         />
-      </Menu>
+      </Menu> */}
     </>
   );
 };
